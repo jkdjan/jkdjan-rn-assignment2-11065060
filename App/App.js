0f042e7b380e6ab9b15be40b862ce3859a7db8ef
kddjan <joelkddjan@gmail.com>
2024-05-27 17:05:29 +0000

    App.js

---
+++ App/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize : 24}}>My name is <Text style={styles.boldText}>Joel Djan</Text> </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#bbb',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
